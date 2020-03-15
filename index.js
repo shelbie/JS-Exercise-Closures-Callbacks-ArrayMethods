947a13c8516fd002badd2e41042e885a5c57d4e8
Shelbie <30550643+shelbie@users.noreply.github.com>
2020-03-15 18:38:48 -0500

    finished processLastItem challenge

--- index.js
+++ index.js
@@ -66,8 +66,9 @@ function processLength(list, callback) {
  * Invoking `processLastItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'barbar'.
 */
-function processLastItem(/* CODE HERE */) {
-  /* CODE HERE */
+function processLastItem(stringList, callback) {
+  let i = 0; i < stringList.length; i++;
+  return callback(stringList[i])
 }
 
 /**
