2cd70ded95c3518bafd0a7b0cf63c2d649580d49
Shelbie Taylor <shelbieltaylor@gmail.com>
2020-03-17 11:01:46 -0500

    1/2 done on getRunnersByTShirtSize

--- index.js
+++ index.js
@@ -294,8 +294,16 @@ function firstNamesAllCaps(runners) {
  * @returns an array containing only the runners that use the given `tShirtSize`.
  * The runners in the array appear in the same order they appear in the `runners` array.
 */
-function getRunnersByTShirtSize(/* CODE HERE */) {
-  /* CODE HERE */
+function getRunnersByTShirtSize(runners, tShirtSize) {
+
+  let tShirts = runners.filter((tShirtSize) => {
+    if(runners === tShirtSize){
+      return true;
+    } else {
+      return false;
+    }
+  })
+  return tShirts;
 }
 
 /**
