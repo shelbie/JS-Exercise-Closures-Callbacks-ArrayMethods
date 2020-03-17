b82d340c66a64fc6fd0221f8679ea05b4c4b55d5
Shelbie Taylor <shelbieltaylor@gmail.com>
2020-03-17 10:36:15 -0500

    finished getFullNames challenge

--- index.js
+++ index.js
@@ -254,8 +254,11 @@ return result;
  * @returns an array with all the runners' full names in the following format: "Smith, John".
  * The full names appear in the array in the same order the runners appear in the `runners` array.
 */
-function getFullNames() {
-  /* CODE HERE */
+function getFullNames(runners) {
+  let fullName = runners.map((name) => {
+    return name.last_name +', ' + name.first_name;
+  })
+  return fullName;
 }
 
 /**
